ceff37e982dd0c86279a75d5a5ee34b2746c1a1f
John <widjaya.john@gmail.com>
2025-02-25 00:17:29 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,17 @@
 import pandas as pd
+import skops.io as sio
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 
+## Loading the Data
 drug_df = pd.read_csv("Data/drug.csv")
 drug_df = drug_df.sample(frac=1)
-drug_df.head(3)
 
+## Train Test Split
 from sklearn.model_selection import train_test_split
 
 X = drug_df.drop("Drug", axis=1).values
@@ -13,12 +21,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=125
 )
 
-from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 
+## Pipeline
 cat_col = [1,2,3]
 num_col = [0,4]
 
@@ -32,32 +36,35 @@ transform = ColumnTransformer(
 pipe = Pipeline(
     steps=[
         ("preprocessing", transform),
-        ("model", RandomForestClassifier(n_estimators=100, random_state=125)),
+        ("model", RandomForestClassifier(n_estimators=10, random_state=125)),
     ]
 )
+
+## Training
 pipe.fit(X_train, y_train)
 
-from sklearn.metrics import accuracy_score, f1_score
 
+## Model Evaluation
 predictions = pipe.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 f1 = f1_score(y_test, predictions, average="macro")
 
 print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
 
-with open("Results/metrics.txt", "w") as outfile:
-    outfile.write(f"\nAccuracy = {accuracy.round(2)}, F1 Score = {f1.round(2)}.")
 
+## Confusion Matrix Plot
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
+predictions = pipe.predict(X_test)
 cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
 disp.plot()
-plt.savefig("Results/model_results.png", dpi=120)
+plt.savefig("./Results/model_results.png", dpi=120)
 
-import skops.io as sio
+## Write metrics to file
+with open("./Results/metrics.txt", "w") as outfile:
+    outfile.write(f"\nAccuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}")
 
-sio.dump(pipe, "Model/drug_pipeline.skops")
-
-sio.load("Model/drug_pipeline.skops", trusted=True)
+## Saving the model file
+sio.dump(pipe, "./Model/drug_pipeline.skops")
